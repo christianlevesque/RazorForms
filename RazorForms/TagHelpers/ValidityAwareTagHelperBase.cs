b6e16b337d8c8f8d78dcab24f8602a8e0c3e81f8
Christian LeVesque <christian@christianlevesque.io>
2022-06-16 11:31:28 -0500

    cleaned up namespace imports

--- RazorForms/TagHelpers/ValidityAwareTagHelperBase.cs
+++ RazorForms/TagHelpers/ValidityAwareTagHelperBase.cs
@@ -1,5 +1,4 @@
-﻿using System.Linq;
-using System.Text.Encodings.Web;
+﻿using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.TagHelpers;
