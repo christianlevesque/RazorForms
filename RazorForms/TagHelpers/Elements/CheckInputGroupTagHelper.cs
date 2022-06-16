1b1e143e6cc44fb94353a8659150874d85afb162
Christian LeVesque <christian@christianlevesque.io>
2022-06-16 11:10:16 -0500

    Added new CheckInputGroupTagHelper and added to example project

---
+++ RazorForms/TagHelpers/Elements/CheckInputGroupTagHelper.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using RazorForms.Generators.Elements;
+using RazorForms.Options.Inputs;
+
+namespace RazorForms.TagHelpers.Elements;
+
+public class CheckInputGroupTagHelper : ValidityAwareTagHelperBase
+{
+	/// <inheritdoc />
+	public CheckInputGroupTagHelper(IHtmlGenerator generator,
+	                                ICheckInputGroupOptions options,
+	                                IInputBlockWrapperGenerator inputBlockWrapperGenerator,
+	                                ILabelGenerator labelGenerator,
+	                                ICheckRadioInputSectionGenerator inputGenerator,
+	                                IErrorGenerator errorGenerator) : base(generator,
+	                                                                       options,
+	                                                                       inputBlockWrapperGenerator,
+	                                                                       labelGenerator,
+	                                                                       inputGenerator,
+	                                                                       errorGenerator)
+	{
+	}
+}
