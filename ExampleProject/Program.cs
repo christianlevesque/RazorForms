4468011f9f5d19d0c639890cebada95de176b177
Christian LeVesque <christian@levesque.dev>
2022-09-28 16:29:00 -0500

    Updated base example to use Bootstrap 5 with floating labels

--- ExampleProject/Program.cs
+++ ExampleProject/Program.cs
@@ -6,47 +6,30 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-builder.Services.UseRazorForms(o =>
+
+builder.Services.UseRazorFormsWithBootstrap5(o =>
 {
-	o.InputOptions.RemoveWrappers = true;
-	o.InputOptions.RenderInputInsideLabel = true;
+	o.InputOptions.ComponentWrapperClasses = "mb-3";
+	o.InputOptions.InputBlockWrapperClasses = "form-floating";
+	o.InputOptions.ErrorWrapperClasses = $"{o.InputOptions.ErrorWrapperClasses} mt-1";
+	o.InputOptions.ErrorClasses = "small";
 	o.InputOptions.InputFirst = true;
-	o.TextAreaOptions.RemoveWrappers = true;
-	o.TextAreaOptions.RenderInputInsideLabel = true;
+	o.InputOptions.RemoveWrappers = true;
+	o.CheckInputGroupOptions.ComponentWrapperClasses = "mb-3";
+	o.RadioInputGroupOptions.ComponentWrapperClasses = "mb-3";
+	o.TextAreaOptions.ComponentWrapperClasses = "mb-3";
+	o.TextAreaOptions.InputBlockWrapperClasses = "form-floating";
+	o.TextAreaOptions.ErrorWrapperClasses = $"{o.InputOptions.ErrorWrapperClasses} mt-1";
+	o.TextAreaOptions.ErrorClasses = "small";
 	o.TextAreaOptions.InputFirst = true;
-	o.SelectOptions.RemoveWrappers = true;
-	o.SelectOptions.RenderInputInsideLabel = true;
+	o.TextAreaOptions.RemoveWrappers = true;
+	o.SelectOptions.ComponentWrapperClasses = "mb-3";
+	o.SelectOptions.InputBlockWrapperClasses = "form-floating";
+	o.SelectOptions.ErrorWrapperClasses = $"{o.SelectOptions.ErrorWrapperClasses} mt-1";
+	o.SelectOptions.ErrorClasses = "small";
 	o.SelectOptions.InputFirst = true;
-	o.CheckInputOptions.RemoveWrappers = true;
-	o.CheckInputOptions.RenderInputInsideLabel = true;
-	o.CheckInputOptions.InputFirst = true;
-	o.RadioInputOptions.RemoveWrappers = true;
-	o.RadioInputOptions.RenderInputInsideLabel = true;
-	o.RadioInputOptions.InputFirst = true;
+	o.SelectOptions.RemoveWrappers = true;
 });
-// builder.Services.UseRazorFormsWithBootstrap(o =>
-// {
-// 	o.InputOptions.ComponentWrapperClasses = "mb-3";
-// 	o.InputOptions.InputBlockWrapperClasses = "form-floating";
-// 	o.InputOptions.ErrorWrapperClasses = $"{o.InputOptions.ErrorWrapperClasses} mt-1";
-// 	o.InputOptions.ErrorClasses = "small";
-// 	o.InputOptions.InputFirst = true;
-// 	o.InputOptions.RemoveWrappers = true;
-// 	o.CheckInputGroupOptions.ComponentWrapperClasses = "mb-3";
-// 	o.RadioInputGroupOptions.ComponentWrapperClasses = "mb-3";
-// 	o.TextAreaOptions.ComponentWrapperClasses = "mb-3";
-// 	o.TextAreaOptions.InputBlockWrapperClasses = "form-floating";
-// 	o.TextAreaOptions.ErrorWrapperClasses = $"{o.InputOptions.ErrorWrapperClasses} mt-1";
-// 	o.TextAreaOptions.ErrorClasses = "small";
-// 	o.TextAreaOptions.InputFirst = true;
-// 	o.TextAreaOptions.RemoveWrappers = true;
-// 	o.SelectOptions.ComponentWrapperClasses = "mb-3";
-// 	o.SelectOptions.InputBlockWrapperClasses = "form-floating";
-// 	o.SelectOptions.ErrorWrapperClasses = $"{o.SelectOptions.ErrorWrapperClasses} mt-1";
-// 	o.SelectOptions.ErrorClasses = "small";
-// 	o.SelectOptions.InputFirst = true;
-// 	o.SelectOptions.RemoveWrappers = true;
-// });
 
 var app = builder.Build();
 
