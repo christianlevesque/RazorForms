57a3cbe4250f99660a4780ab517eae885a33a317
Christian LeVesque <christian@levesque.dev>
2022-12-20 17:27:23 -0600

    Removed unused utility method

--- RazorForms/Utilities.cs
+++ RazorForms/Utilities.cs
@@ -10,20 +10,6 @@ namespace RazorForms;
 public static class Utilities
 {
 	/// <summary>
-	/// Gets the <see cref="DescriptionAttribute"/> value associated with an enum member. If it does not exist, it returns the enum member stringified.
-	/// </summary>
-	/// <param name="e"></param>
-	/// <returns></returns>
-	public static string GetDescription(this Enum e)
-	{
-		var attr = e.GetType()
-		            .GetField(e.ToString())?
-		            .GetCustomAttributes(typeof (DescriptionAttribute), false)
-		            .FirstOrDefault() as DescriptionAttribute;
-		return attr?.Description ?? e.ToString();
-	}
-
-	/// <summary>
 	/// Appends a value to the given <see cref="StringBuilder"/> instance, including a leading space if the instance is not currently empty
 	/// </summary>
 	/// <param name="self">the <see cref="StringBuilder"/> instance</param>
