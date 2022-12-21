f4c092b4351c85b33e1f679faced1a8d096077be
Christian LeVesque <christian@levesque.dev>
2022-12-20 19:10:54 -0600

    changed UseRazorForms(RazorFormsOptions) extension method to do checking for template strings so other methods don't have to

--- RazorForms/Microsoft.Extensions.DependencyInjection/RazorFormsExtensions.cs
+++ RazorForms/Microsoft.Extensions.DependencyInjection/RazorFormsExtensions.cs
@@ -16,9 +16,44 @@ public static class RazorFormsExtensions
 	/// <param name="self">The <see cref="IServiceCollection"/> instance</param>
 	/// <param name="options">The options to use when creating markup</param>
 	/// <returns></returns>
-	public static IServiceCollection UseRazorForms(this IServiceCollection self, RazorFormsOptions options)
+	public static IServiceCollection UseRazorForms(this IServiceCollection self, RazorFormsOptions o)
 	{
-		return self.AddSingleton(options);
+		if (string.IsNullOrWhiteSpace(o.TextInputOptions.TemplatePath))
+		{
+			o.TextInputOptions.TemplatePath = ValidityAwareContentPartial;
+		}
+
+		if (string.IsNullOrWhiteSpace(o.TextAreaInputOptions.TemplatePath))
+		{
+			o.TextAreaInputOptions.TemplatePath = ValidityAwareContentPartial;
+		}
+
+		if (string.IsNullOrWhiteSpace(o.SelectInputOptions.TemplatePath))
+		{
+			o.SelectInputOptions.TemplatePath = ValidityAwareContentPartial;
+		}
+
+		if (string.IsNullOrWhiteSpace(o.CheckInputGroupOptions.TemplatePath))
+		{
+			o.CheckInputGroupOptions.TemplatePath = ValidityAwareContentPartial;
+		}
+
+		if (string.IsNullOrWhiteSpace(o.RadioInputGroupOptions.TemplatePath))
+		{
+			o.RadioInputGroupOptions.TemplatePath = ValidityAwareContentPartial;
+		}
+
+		if (string.IsNullOrWhiteSpace(o.CheckInputOptions.TemplatePath))
+		{
+			o.CheckInputOptions.TemplatePath = ContentPartial;
+		}
+
+		if (string.IsNullOrWhiteSpace(o.RadioInputOptions.TemplatePath))
+		{
+			o.RadioInputOptions.TemplatePath = ContentPartial;
+		}
+
+		return self.AddSingleton(o);
 	}
 
 	/// <summary>
@@ -29,37 +64,7 @@ public static class RazorFormsExtensions
 	/// <returns></returns>
 	public static IServiceCollection UseRazorForms(this IServiceCollection self, Action<RazorFormsOptions> action)
 	{
-		var options = new RazorFormsOptions
-		{
-			CheckInputOptions =
-			{
-				TemplatePath = ContentPartial
-			},
-			CheckInputGroupOptions =
-			{
-				TemplatePath = ValidityAwareContentPartial
-			},
-			RadioInputOptions =
-			{
-				TemplatePath = ContentPartial
-			},
-			RadioInputGroupOptions =
-			{
-				TemplatePath = ValidityAwareContentPartial
-			},
-			TextInputOptions =
-			{
-				TemplatePath = ValidityAwareContentPartial
-			},
-			TextAreaInputOptions = 
-			{
-				TemplatePath = ValidityAwareContentPartial
-			},
-			SelectInputOptions = 
-			{
-				TemplatePath = ValidityAwareContentPartial
-			}
-		};
+		var options = new RazorFormsOptions();
 		action(options);
 		return self.UseRazorForms(options);
 	}
