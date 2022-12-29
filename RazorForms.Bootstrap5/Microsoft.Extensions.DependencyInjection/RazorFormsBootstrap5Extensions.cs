9480fd678d0a11a4129d4b9b365eabb9c7eae2d3
Christian LeVesque <christian@levesque.dev>
2022-12-29 12:28:38 -0600

    finalized Bootstrap5 package

--- RazorForms.Bootstrap5/Microsoft.Extensions.DependencyInjection/RazorFormsBootstrap5Extensions.cs
+++ RazorForms.Bootstrap5/Microsoft.Extensions.DependencyInjection/RazorFormsBootstrap5Extensions.cs
@@ -15,35 +15,20 @@ public static class RazorFormsBootstrap5Extensions
 	/// </remarks>
 	/// <param name="self">The <see cref="IServiceCollection"/> instance</param>
 	/// <returns></returns>
-	public static IServiceCollection UseRazorFormsWithBootstrap5<T>(this IServiceCollection self)
-		where T : RazorFormsOptions, new() => self.UseRazorForms<T>(ApplyBootstrapDefaults);
+	public static IServiceCollection UseRazorFormsWithBootstrap5(this IServiceCollection self)
+		=> self.UseRazorForms<RazorFormsOptions>(ApplyBootstrapDefaults);
 
 	/// <summary>
 	/// Adds RazorForms support with configurable Bootstrap 5 settings
 	/// </summary>
 	/// <remarks>
-	/// Use this overload when you want to include customizable Bootstrap 5 support.
+	/// Use this overload when you want to include customizable Bootstrap 5 support with a custom subclass of <see cref="RazorFormsOptions"/>.
 	/// </remarks>
 	/// <param name="self">The <see cref="IServiceCollection"/> instance</param>
-	/// <param name="action">An <see cref="Action"/> that can be used to mutate the default Bootstrapv5 options</param>
+	/// <param name="action">An <see cref="Action"/> that can be used to mutate the default Bootstrap5 options</param>
 	/// <returns></returns>
-	public static IServiceCollection UseRazorFormsWithBootstrap5<T>(this IServiceCollection self, Action<T> action)
-		where T : RazorFormsOptions, new()
-	{
-		var bootstrap = new T();
-		action(bootstrap);
-		ApplyBootstrapDefaults(bootstrap);
-
-		return self.UseRazorForms(bootstrap);
-	}
-
-	/// <summary>
-	/// Add RazorForms support with configurable Bootstrap 5 settings, along with added configuration to set up Bootstrap floating form labels
-	/// </summary>
-	/// <param name="self">The <see cref="IServiceCollection"/> instance</param>
-	/// <param name="action">An <see cref="Action"/> that can be used to mutate the default Bootstrapv5 options</param>
-	/// <returns></returns>
-	public static IServiceCollection UseRazorFormsWithBootstrap5FloatingLabels<T>(this IServiceCollection self,
+	public static IServiceCollection UseRazorFormsWithBootstrap5<T>(
+		this IServiceCollection self,
 		Action<T> action)
 		where T : RazorFormsOptions, new()
 	{
@@ -51,23 +36,74 @@ public static class RazorFormsBootstrap5Extensions
 		action(bootstrap);
 		ApplyBootstrapDefaults(bootstrap);
 
-		// Inputs
-		bootstrap.TextInputOptions.InputBlockWrapperClasses = Utilities.MergeCssStrings("form-floating", bootstrap.TextInputOptions.InputBlockWrapperClasses);
-		bootstrap.TextInputOptions.InputFirst = true;
-		bootstrap.TextInputOptions.RemoveWrappers = true;
+		return self.UseRazorForms(bootstrap);
+	}
 
-		// TextAreas
-		bootstrap.TextAreaInputOptions.InputBlockWrapperClasses = Utilities.MergeCssStrings("form-floating", bootstrap.TextAreaInputOptions.InputBlockWrapperClasses);
-		bootstrap.TextAreaInputOptions.InputFirst = true;
-		bootstrap.TextAreaInputOptions.RemoveWrappers = true;
+	/// <summary>
+	/// Adds RazorForms support with configurable Bootstrap 5 settings
+	/// </summary>
+	/// <remarks>
+	/// Use this overload when you want to include customizable Bootstrap 5 support with the built-in <see cref="RazorFormsOptions"/>.
+	/// </remarks>
+	/// <param name="self">The <see cref="IServiceCollection"/> instance</param>
+	/// <param name="action">An <see cref="Action"/> that can be used to mutate the default Bootstrap5 options</param>
+	/// <returns></returns>
+	public static IServiceCollection UseRazorFormsWithBootstrap5(
+		this IServiceCollection self,
+		Action<RazorFormsOptions> action)
+		=> UseRazorFormsWithBootstrap5<RazorFormsOptions>(self, action);
 
-		// Selects
-		bootstrap.SelectInputOptions.InputBlockWrapperClasses = Utilities.MergeCssStrings("form-floating", bootstrap.SelectInputOptions.InputBlockWrapperClasses);
-		bootstrap.SelectInputOptions.InputFirst = true;
-		bootstrap.SelectInputOptions.RemoveWrappers = true;
+	/// <summary>
+	/// Add RazorForms support with default Bootstrap 5 settings, along with added configuration to set up Bootstrap floating form labels
+	/// </summary>
+	/// <remarks>
+	/// Use this overload when you want to include basic support for Bootstrap 5 with floating labels. This method doesn't allow any customization.
+	/// </remarks>
+	/// <param name="self">The <see cref="IServiceCollection"/> instance</param>
+	/// <param name="action">An <see cref="Action"/> that can be used to mutate the default Bootstrap5 options</param>
+	/// <returns></returns>
+	public static IServiceCollection UseRazorFormsWithBootstrap5FloatingLabels(this IServiceCollection self)
+		=> self.UseRazorForms<RazorFormsOptions>(o =>
+		{
+			ApplyBootstrapDefaults(o);
+			ApplyBootstrapFloatingLabel(o);
+		});
+
+	/// <summary>
+	/// Add RazorForms support with configurable Bootstrap 5 settings, along with added configuration to set up Bootstrap floating form labels
+	/// </summary>
+	/// <remarks>
+	/// Use this overload when you want to include customizable Bootstrap 5 support with a custom subclass of <see cref="RazorFormsOptions"/>.
+	/// </remarks>
+	/// <param name="self">The <see cref="IServiceCollection"/> instance</param>
+	/// <param name="action">An <see cref="Action"/> that can be used to mutate the default Bootstrap5 options</param>
+	/// <returns></returns>
+	public static IServiceCollection UseRazorFormsWithBootstrap5FloatingLabels<T>(
+		this IServiceCollection self,
+		Action<T> action)
+		where T : RazorFormsOptions, new()
+	{
+		var bootstrap = new T();
+		action(bootstrap);
+		ApplyBootstrapDefaults(bootstrap);
+		ApplyBootstrapFloatingLabel(bootstrap);
 
 		return self.UseRazorForms(bootstrap);
 	}
+
+	/// <summary>
+	/// Add RazorForms support with configurable Bootstrap 5 settings, along with added configuration to set up Bootstrap floating form labels
+	/// </summary>
+	/// <remarks>
+	/// Use this overload when you want to include customizable Bootstrap 5 support with the built-in <see cref="RazorFormsOptions"/>.
+	/// </remarks>
+	/// <param name="self">The <see cref="IServiceCollection"/> instance</param>
+	/// <param name="action">An <see cref="Action"/> that can be used to mutate the default Bootstrap5 options</param>
+	/// <returns></returns>
+	public static IServiceCollection UseRazorFormsWithBootstrap5FloatingLabels(
+		this IServiceCollection self,
+		Action<RazorFormsOptions> action)
+		=> UseRazorFormsWithBootstrap5FloatingLabels<RazorFormsOptions>(self, action);
 
 	public static void ApplyBootstrapDefaults<T>(T o)
 		where T : RazorFormsOptions
@@ -127,5 +163,30 @@ public static class RazorFormsBootstrap5Extensions
 		o.RadioInputGroupOptions.LabelValidClasses = Utilities.MergeCssStrings("text-success", o.RadioInputGroupOptions.LabelValidClasses);
 		o.RadioInputGroupOptions.LabelInvalidClasses = Utilities.MergeCssStrings("text-danger", o.RadioInputGroupOptions.LabelInvalidClasses);
 		o.RadioInputGroupOptions.ErrorWrapperClasses = Utilities.MergeCssStrings("text-danger list-unstyled", o.RadioInputGroupOptions.ErrorWrapperClasses);
+	}
+
+	public static void ApplyBootstrapFloatingLabel<T>(T o)
+		where T : RazorFormsOptions
+	{
+		// Inputs
+		o.TextInputOptions.InputBlockWrapperClasses = Utilities.MergeCssStrings(
+			"form-floating",
+			o.TextInputOptions.InputBlockWrapperClasses);
+		o.TextInputOptions.InputFirst = true;
+		o.TextInputOptions.RemoveWrappers = true;
+
+		// TextAreas
+		o.TextAreaInputOptions.InputBlockWrapperClasses = Utilities.MergeCssStrings(
+			"form-floating",
+			o.TextAreaInputOptions.InputBlockWrapperClasses);
+		o.TextAreaInputOptions.InputFirst = true;
+		o.TextAreaInputOptions.RemoveWrappers = true;
+
+		// Selects
+		o.SelectInputOptions.InputBlockWrapperClasses = Utilities.MergeCssStrings(
+			"form-floating",
+			o.SelectInputOptions.InputBlockWrapperClasses);
+		o.SelectInputOptions.InputFirst = true;
+		o.SelectInputOptions.RemoveWrappers = true;
 	}
 }
